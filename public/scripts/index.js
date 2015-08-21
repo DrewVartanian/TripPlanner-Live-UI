c1969908aeea0a83c33d78fd940aca1906dc9197
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-08-21 11:43:25 -0400

    "Finished non bonus map"

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,43 +9,58 @@ $(document).ready(function(){
   $('#activity-panel').on('click','button',function(){
     var activity_type = $(this).attr("data-type");
     var activity_name = $(this).prev().val();
+    var marker;
     if(activity_type=='hotel'){
       $('ul.'+activity_type).html('');
-      agenda.days[agenda.selected].hotel=[activity_name];
+      marker = addToMap(activity_name,activity_type);
+      agenda.days[agenda.selected].hotel=[new Activity(activity_name,marker)];
     }else{
-       var arr = $('ul.'+activity_type+' span').toArray();
-       if(arr.some(function(span){
+      var arr = $('ul.'+activity_type+' span').toArray();
+      if(arr.some(function(span){
         return $(span).text() === activity_name;
-       })) return;
-      agenda.days[agenda.selected][activity_type].push(activity_name);
+      })) return;
+      marker = addToMap(activity_name,activity_type);
+      agenda.days[agenda.selected][activity_type].push(new Activity(activity_name,marker));
     }
     $('ul.'+activity_type).append($(genNewAddition(activity_name,activity_type)));
+    zoom();
   });
 
   $('#add-day').on('click',function(){
     agenda.days.push(new Day());
-    $(this).before('<button class="btn btn-circle day-btn day">'
-                   +(agenda.days.length)+'</button>');
+    $(this).before('<button class="btn btn-circle day-btn day">'+
+                   (agenda.days.length)+'</button>');
   });
 
   $('#agenda-panel .panel-body').on('click','button',function(){
     $(this).parent().remove();
     var type = $(this).attr('data-type');
     var arr = agenda.days[agenda.selected][type];
-    var i = arr.indexOf($(this).prev().text());
+    var i;
+    for(var j=0;j<arr.length;j++){
+      if(arr[j].name===$(this).prev().text()){
+        i=j;
+        break;
+      }
+    }
+    arr[i].marker.setMap(null);
     agenda.days[agenda.selected][type] =
       arr.slice(0,i).concat(arr.slice(i+1));
+    zoom();
   });
 
   $('.day-buttons').on('click','.day',function(){
+    clearMarkers();
     $('.day-buttons .current-day').removeClass('current-day');
     $(this).addClass('current-day');
     agenda.selected = Number($(this).text())-1;
     $('#day-title span').text('Day '+(agenda.selected+1));
     updateDay();
+    zoom();
   });
 
   $('#day-title').on('click','button',function(){
+    clearMarkers();
     if(agenda.days.length===1){
       for(var key in agenda.days[agenda.selected]){
         $('#agenda-panel .'+key).html('');
@@ -66,8 +81,8 @@ $(document).ready(function(){
                 .concat(agenda.days.slice(agenda.selected+1));
     }
 
-    console.log("update");
     updateDay();
+    zoom();
   });
 });
 
@@ -77,12 +92,72 @@ function genNewAddition(name,type){
 }
 
 function updateDay(){
+  var cbFunc= function(html, activity){
+    activity.marker.setMap(map);
+    return html + genNewAddition(activity.name,key);
+  };
   for(var key in agenda.days[agenda.selected]){
-    var html = '';
-    agenda.days[agenda.selected][key].forEach(function(name){
-      html+=genNewAddition(name,key);
-    });
+    var html=agenda.days[agenda.selected][key].reduce(cbFunc,'');
     $('#agenda-panel .'+key).html(html);
+  }
+}
+
+function addToMap(name,type){
+  var all;
+  var icon;
+  switch(type){
+    case 'hotel':
+      all=all_hotels;
+      icon = {icon: '/images/lodging_0star.png'};
+      break;
+    case 'restaurant':
+      all=all_restaurants;
+      icon = {icon: '/images/restaurant.png'};
+      break;
+    default:
+      all=all_activities;
+      icon = {icon: '/images/star-3.png'};
+  }
+  for(var i=0;i<all.length;i++){
+    if(all[i].name===name){
+      var marker = drawLocation(all[i].place[0].location,icon);
+      // agenda.days[agenda.selected].bouinds
+      return marker;
+    }
+  }
+}
+
+function drawLocation (location, opts) {
+  if (typeof opts !== 'object') {
+    opts = {};
+  }
+  opts.position = new google.maps.LatLng(location[0], location[1]);
+  opts.map = map;
+  return new google.maps.Marker(opts);
+}
+
+function clearMarkers(){
+  var day = agenda.days[agenda.selected];
+  for(var key in day){
+    day[key].forEach(function(activity){
+      activity.marker.setMap(null);
+    });
+  }
+}
+
+function zoom(){
+  var day = agenda.days[agenda.selected];
+  var bounds = new google.maps.LatLngBounds();
+  var markerFound = false;
+  var cbFunc = function(activity){
+    markerFound = true;
+    bounds.extend(activity.marker.position);
+  };
+  for(var key in day){
+    day[key].forEach(cbFunc);
+  }
+  if(markerFound){
+    map.fitBounds(bounds);
   }
 }
 
@@ -90,4 +165,9 @@ function Day(){
   this.hotel = [];
   this.restaurant = [];
   this.activity = [];
+}
+
+function Activity(name,marker){
+  this.name = name;
+  this.marker= marker;
 }
